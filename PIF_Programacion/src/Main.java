4925cf4726ec62b48b058ab90f6ad7ca331b52e6
Evan-Roams <evanroamsenterprices@gmail.com>
2024-04-11 00:11:58 -0500

    saveee

--- PIF_Programacion/src/Main.java
+++ PIF_Programacion/src/Main.java
@@ -5,35 +5,112 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner Teclado = new Scanner(System.in);
+        int Programa;
 
-        // Main - Calculadora turnos por hora
-        System.out.println("---CALCULADOR DE TURNO SEGÚN HORA---");
-            byte hora;
-            byte minutos;
-            byte turno;
+        //Buses y Asientos
+        int Buses = 68;
+        int Asientos = 50;
+        byte Cupos;
 
+        //Turnos y Horas  y Documentos
+        byte Hora;
+        byte Minutos;
+        byte Turno;
+        int Documento;
+
+        //Reservas
+        int reservas[][] = new int[Buses][Asientos];
+        int copia_reservas[][] = new int[reservas.length][reservas[0].length];
+        byte Reservas_Usuario[];
+        reservas[45][49]=1010;
+        copia_reservas = copiar_reservas(reservas);
+
+        System.out.println("-----Programas / Tareas-----");
+        System.out.println("1-Calculadora de Turno según hora");
+        System.out.println("2-Copiadora de Matrices");
+        System.out.println("3-Contadora de Cupos");
+        System.out.println("4-Turnos de Buses de Usuario");
+        System.out.print("Que desea hacer? (digite el numero del programa o tarea:");
+        Programa = Teclado.nextInt();
+        System.out.println("");
+
+
+        if (Programa==1){
+            // Main - Calculadora turnos por hora
+            System.out.println("---CALCULADOR DE TURNO SEGÚN HORA---");
             System.out.print("Escriba su Hora (de 0 a 22):");
-            hora = Teclado.nextByte();
-            if (hora<23){
-                System.out.println("Escrba sus Minutos (de 0 a 59): ");
-                minutos = Teclado.nextByte();
-                if (minutos<60){
+            Hora = Teclado.nextByte();
+            if (Hora<23){
+                System.out.println("Escriba sus Minutos (de 0 a 59): ");
+                Minutos = Teclado.nextByte();
+                if (Minutos<60){
 
-                    turno = determinar_turno(hora, minutos);
-                    System.out.println("Su turno es el: "+(turno));
+                    Turno = determinar_turno(Hora, Minutos);
+                    System.out.println("Su turno es el: "+(Turno));
                 } else {
                     System.out.print("introduzca unos minutos válidos (de 0 a 59)");
                 }
             } else {
-               System.out.print("introduzca una hora válida (de 0 a 22)");
+                System.out.print("introduzca una hora válida (de 0 a 22)");
             }
 
 
-        // Main - Calculadora turnos por hora
-        System.out.println("---CALCULADOR DE TURNO SEGÚN HORA---");
+        } else if (Programa==2){
+            // Main - Calculadora turnos por hora
+            System.out.println("---COPIADORA DE MATRICES---");
 
-        int reservas[][] = new int[68][50];
-        int copiar_reservas[][] = new int[68][50];
+            System.out.println("Matriz a Copiar");
+
+            for (int i = 0; i<reservas.length; i++){
+                for (int x=0; x<reservas[0].length; x++){
+                    System.out.print(" "+reservas[i][x]);
+                }
+                System.out.println(" ");
+            }
+            System.out.println(" ");
+
+            System.out.println("Matriz Copiada");
+            for (int i = 0; i<copia_reservas.length; i++){
+                for (int x=0; x<copia_reservas[0].length; x++){
+                    System.out.print(" "+copia_reservas[i][x]);
+                }
+                System.out.println(" ");
+            }
+
+
+        } else if (Programa==3){
+            // Main - Contadora de Cupos
+            System.out.println("---CONTADORA DE CUPOS---");
+
+            System.out.print("Escriba su Hora (de 0 a 22):");
+            Hora = Teclado.nextByte();
+            if (Hora<23){
+                System.out.println("Escriba sus Minutos (de 0 a 59): ");
+                Minutos = Teclado.nextByte();
+                if (Minutos<60){
+
+                    Turno = determinar_turno(Hora, Minutos);
+                    Cupos = contar_cupos_bus(Hora, Minutos, reservas);
+                    System.out.println("Quedan "+Cupos+" Asientos disponibles para el turno "+Turno+".");
+                } else {
+                    System.out.print("introduzca unos minutos válidos (de 0 a 59)");
+                }
+            } else {
+                System.out.print("introduzca una hora válida (de 0 a 22)");
+            }
+
+        } else if (Programa==4) {
+            System.out.print("Introduzca su Número de Documento:");
+            Documento = Teclado.nextInt();
+            System.out.println(" ");
+
+            Reservas_Usuario = buscar_reservas(Documento, reservas );
+            System.out.print("Los turnos en los que está el usuarios son: [|");
+            for (byte i = 0; i< Reservas_Usuario.length; i++){
+                System.out.print(Reservas_Usuario[i]+"|");
+            }
+            System.out.print("] ");
+        }
 
 
     }
@@ -57,11 +134,49 @@ public class Main {
 
     // Método para copiar una matriz
     static int[][] copiar_reservas (int[][] reservas) {
-        int copiar_reservas[][] = new int[68][50];
+        int copiar_reservas[][] = new int[reservas.length][reservas[0].length];
 
-        for ()
+        // Mediante dos ciclos se recorre la nueva matriz y se va reemplazando sus
+        // filas y columnas con las filas y columnas de la matriz original.
+        for (int i = 0; i<reservas.length; i++){
+            for (int x = 0; x<reservas[0].length; x++){
+                copiar_reservas[i][x] = reservas[i][x];
+            }
+        }
 
         return copiar_reservas;
+    }
+
+    // Método para cupos libres restantes en un bus
+    static byte contar_cupos_bus(byte hora, byte minutos, int[][] reservas) {
+        byte cupos = 0;
+        byte turno = determinar_turno(hora, minutos);
+
+        for (byte i = 0; i < reservas[0].length; i++){
+            if (reservas[turno][i] == 0){
+                cupos++;
+            }
+        }
+
+
+        return cupos;
+    }
+
+    // Método para buscar las reservas de un Usuario
+    static byte[] buscar_reservas(int documento, int[][] reservas) {
+
+        byte reservas_usuario[] = new byte[reservas.length];
+
+        for (byte i = 0; i<reservas.length; i++){
+            for (byte x = 0; x<reservas[0].length; x++){
+                if (reservas[i][x] == documento){
+                    reservas_usuario[i] = i;
+
+                }
+            }
+        }
+
+        return reservas_usuario;
     }
 
 
